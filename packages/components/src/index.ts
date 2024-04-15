39ea9e233c28a897a36a84de2a46a3a63850dc26
wuxi <1047605917@qq.com>
2024-04-15 10:36:26 +0800

    feat: 新增面包屑 菜单导航功能

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -12,6 +12,17 @@ import PageContainer from './components/PageContainer';
 import ProTable from './components/ProTable';
 import ProForm from './components/ProForm';
 import AdForm, { FormColumnsTypes, FornPropsTypes } from './components/ProForm/form';
-export { Header, OutletLayoutRouter, Layout, Text, PageContainer, ProTable, ProForm, AdForm };
+export {
+  Header,
+  OutletLayoutRouter,
+  Layout,
+  Text,
+  PageContainer,
+  ProTable,
+  ProForm,
+  AdForm,
+  Breadcrumb,
+  TabCom,
+};
 
 export type { MenuItem, FormColumnsTypes, FornPropsTypes };
