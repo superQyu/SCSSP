4c03ec54d37f1b50fc58210c604650edea426d2a
wuxi <1047605917@qq.com>
2024-04-15 14:39:52 +0800

    merge:合并代码

--- apps/admin/src/pages/systemManagement/roleManagement/index.tsx
+++ apps/admin/src/pages/systemManagement/roleManagement/index.tsx
@@ -6,6 +6,8 @@ import { useRef } from 'react';
 import { getGithubIssueItem } from 'apis';
 import type { AdminApi } from 'apis';
 
+
+const  temp = "3";
 const columns: ProColumns<AdminApi.GithubIssueItem>[] = [
   {
     dataIndex: 'index',
